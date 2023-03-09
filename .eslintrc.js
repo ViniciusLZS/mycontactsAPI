704eef62cf6ca2676bf767f0a1ca5524a46c1b04
Vinícius <vinicius.luiz.9256@gmail.com>
2023-03-08 22:11:04 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'class-methods-use-this': 'off',
+    'consistent-return': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['erro', { argsIgnorePattern: 'next' }],
+  },
+};
